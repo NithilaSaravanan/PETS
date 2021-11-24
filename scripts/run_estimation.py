9993199a2d3cbdc967c836d41c358e0b4deae576
NithilaSaravanan <70170680+NithilaSaravanan@users.noreply.github.com>
2021-11-24 01:09:27 -0500

    Create run_estimation.py

---
+++ scripts/run_estimation.py
@@ -0,0 +1,7 @@
+#Plugging in an empty script here - to be filled in 
+
+def main():
+	pass
+	print('will get parameters from config files, input from the data folde and then run the codes')
+if __name__=='__main__':
+	main()
